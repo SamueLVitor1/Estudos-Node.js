4cf6f333922696de9c590806150b6a6488e51388
Samuel Vitor <samuelvitor668@gmail.com>
2024-01-22 21:18:49 -0300

    Projeto 1 -  rotas e metodos HTTP

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,7 +1,15 @@
 import http from 'node:http';
 
 const server = http.createServer((request, response)=>{
+  const {method, url} = request
+  
+  if(method === 'GET' && url === '/users') {
+    return response.end('listagem de usuarios')
+  }
 
+  if(method === 'POST' && url === '/users') {
+    return response.end('criação de usuarios')
+  }
 })
 
 server.listen(3333)
